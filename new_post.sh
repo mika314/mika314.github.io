64ac7b5e589bcfe1319937dfb76563e0ac59fa06
Mika Pi <mika@mika.global>
2021-10-08 22:29:47 -0700

    autoupdate

---
+++ new_post.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+datetime=`date +'%Y-%m-%d %H:%m:%S'`
+filename=`date +'%Y-%m-%d-%s'`.md
+echo $filename
+echo "---
+Layout: post
+title: $datetime
+date: $datetime +0700
+---" > "_posts/$filename"
+emacs "_posts/$filename"
+git add .
+git commit -m "autoupdate"
+git pull
+git push
